c6fc0fd5e9133f3fd397d62f543b6eebb7fbad56
Faizan Karim <50496663+TheDarkLach@users.noreply.github.com>
2020-11-22 15:27:12 -0800

    mann

--- main.cpp
+++ main.cpp
@@ -112,6 +112,96 @@ int main()
     {
       PRINT(list, size);
     }
+    else if (strcmp(input, "addrand") == 0) {  //add random student
+      int num;
+      //initialize name arrays
+      char** namef = new char*[20];
+      char** namel = new char*[20];
+      for (int i = 0; i < 20; i++) {
+	namef[i] = new char[81];
+	namel[i] = new char[81];
+      }
+      cout << endl << "How many random students should be added: ";
+      cin >> num;
+      cin.clear();
+      cin.ignore(10000, '\n');
+      ifstream myfile("fnames.txt");
+      int linenr = 0;
+      char first[81];
+      while (linenr < 20) {
+	myfile.getline(first, 81);
+	cout << first << linenr << endl;
+	strcpy(namef[linenr], first);
+	linenr++;
+      }
+      ifstream myfile1("lnames.txt");
+      char last[81];
+      linenr = 0; //restar count
+      while (linenr < 20) {
+	myfile1.getline(last, 81);
+	strcpy(namel[linenr], last);
+	linenr++;
+      }
+      while (num > 0) {
+	student* newStudent = new student();  //initalize new student
+	//make random numbers between 0 and 19
+	int randomf = rand() % 19;
+	int randoml = rand() % 19;
+	//assign random data to new studen
+	strcpy(newStudent->Fname, namef[randomf]);
+	strcpy(newStudent->Lname, namel[randoml]);
+	newStudent->id = randid;
+	newStudent->gpa = (float)rand()/(RAND_MAX)*5;
+	//increment by 100
+	randid = randid + 100;
+	      
+	//add new student genrated
+	ADD(list, newStudent, size);
+	//balance if collision
+	if (COLL(list, size)) {
+	  cout << endl << ">>Rebalancing hash table array to double the size." << endl;
+	  student** temp = new student*[size];  //create temp array
+	  for (int m = 0; m < size; m++) {
+	    temp[m] = list[m];
+	  }
+	  int newsize = 2*size;
+	  list = new student*[newsize]; //recreate list to double
+	  for (int c = 0; c < newsize; c++) {
+	    list[c] = NULL;
+	  }
+	  //re-add all students to newly sized list
+	  for (int a = 0; a < size; a++) {
+	    if (temp[a] != NULL) {
+	      student* move = temp[a];
+	      if (move->next != NULL) {
+		student* nxt = move->next;
+		move->next = NULL;
+		nxt->prev = NULL;
+		ADD(list, nxt, newsize);
+		if (nxt->next != NULL) {
+		  student* dnxt = nxt->next;
+		  nxt->next = NULL;
+		  dnxt->prev = NULL;
+		  ADD(list, dnxt, newsize);
+		}
+	      }
+            ADD(list, move, newsize);
+	    }
+	  }
+	  delete[] temp;
+	  size = newsize; //update list's size
+	}
+	num--;
+      }
+    }
+    else if (strcmp(input, "quit") == 0) {
+      cout << endl << "Quitting" << endl;
+      run = false;
+    } 
+    else 
+    {
+      cout << endl << "Invalid input. Try again." << endl;
+    }
   }
    return 0;
 }
