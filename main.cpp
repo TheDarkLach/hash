a4aea1247e10fcbd65db6d94a32d02ffb0cbbe5d
Faizan Karim <50496663+TheDarkLach@users.noreply.github.com>
2020-11-22 16:42:33 -0800

    dsfsd

--- main.cpp
+++ main.cpp
@@ -11,20 +11,20 @@
 
 using namespace std;
 
-struct stud {
+struct student {
   char Fname[81];
   char Lname[81];
   int id;
   float gpa;
-  stud* next;
-  stud* prev;
+  student* next;
+  student* prev;
 };
 
 //functions
-void ADD(stud** list, stud* newStud, int size);
-void PRINT(stud** list, int size);
-void REMOVE(stud** list, int id, int size);
-bool COLL(stud** list, int size);
+void ADD(student** list, student* newStudent, int size);
+void PRINT(student** list, int size);
+void REMOVE(student** list, int id, int size);
+bool COLL(student** list, int size);
 
 int main() {
   //variables
@@ -32,7 +32,7 @@ int main() {
   char input[10];
   int randid = 123; //keeps track or random id value
   //hash table variabes
-  stud** list = new stud*[100];
+  student** list = new student*[100];
   int size = 100;
   while (run) {
     cout << endl << "You can: add, addrand, remove, print, quit." << endl;
@@ -41,37 +41,37 @@ int main() {
     cin.ignore(10000, '\n');
     if (strcmp(input, "add") == 0) {  //add student manually
       //get info from user input
-      stud* newStud = new stud();
-      newStud->next = NULL;
-      newStud->prev = NULL;
+      student* newStudent = new student();
+      newStudent->next = NULL;
+      newStudent->prev = NULL;
       cout << "Enter first name: " << endl;
-      cin >> newStud->Fname;
+      cin >> newStudent->Fname;
       cin.clear();
       cin.ignore(81,'\n');
       cout << "Enter last name: " << endl;
-      cin >> newStud->Lname;
+      cin >> newStudent->Lname;
       cin.clear();
       cin.ignore(81,'\n');
       cout << "Enter ID: " << endl;
-      cin >> newStud->id;
+      cin >> newStudent->id;
       cin.clear();
       cin.ignore(81,'\n');
       cout << "Enter GPA: " << endl;
-      cin >> newStud->gpa;
+      cin >> newStudent->gpa;
       cin.clear();
       cin.ignore(81, '\n');
-      ADD(list, newStud, size);
+      ADD(list, newStudent, size);
       cout << endl << "New student added to hash table." << endl;
       //balance if collision
       if (COLL(list, size)) {
 	cout << endl << "Changing hash table array to double the size." << endl;
-	stud** temp = new stud*[size];  //create temp array
+	student** temp = new student*[size];  //create temp array
 	for (int m = 0; m < size; m++) 
 	{
 	  temp[m] = list[m];
 	}
 	int newsize = 2*size;
-	list = new stud*[newsize]; //recreate list to double
+	list = new student*[newsize]; //recreate list to double
 	for (int c = 0; c < newsize; c++) 
 	{
           list[c] = NULL;
@@ -80,16 +80,16 @@ int main() {
 	for (int a = 0; a < size; a++) 
 	{
 	  if (temp[a] != NULL) {
-	    stud* move = temp[a];
+	    student* move = temp[a];
 	    if (move->next != NULL) 
 	    {
-	      stud* nxt = move->next;
+	      student* nxt = move->next;
 	      move->next = NULL;
 	      nxt->prev = NULL;
 	      ADD(list, nxt, newsize);
 	      if (nxt->next != NULL) 
 	      {
-		stud* dnxt = nxt->next;
+		student* dnxt = nxt->next;
 		nxt->next = NULL;
 		dnxt->prev = NULL;
 		ADD(list, dnxt, newsize);
@@ -149,41 +149,41 @@ int main() {
       }
       while (num > 0) 
       {
-	stud* newStud = new stud();  //initalize new student
+	student* newStudent = new student();  //initalize new student
 	//make random numbers between 0 and 19
 	int randomf = rand() % 19;
 	int randoml = rand() % 19;
 	//assign random data
-	strcpy(newStud->Fname, namef[randomf]);
-	strcpy(newStud->Lname, namel[randoml]);
-	newStud->id = randid;
+	strcpy(newStudent->Fname, namef[randomf]);
+	strcpy(newStudent->Lname, namel[randoml]);
+	newStudent->id = randid;
 	newStud->gpa = (float)rand()/(RAND_MAX)*5;
 	randid = randid + 100;
 	//add new student genrated
-	ADD(list, newStud, size);
+	ADD(list, newStudent, size);
 	//balance if collision
 	if (COLL(list, size)) {
 	  cout << endl << "Changing hash table array to double the size." << endl;
-	  stud** temp = new stud*[size];  //create temp array
+	  student** temp = new student*[size];  //create temp array
 	  for (int m = 0; m < size; m++) {
 	    temp[m] = list[m];
 	  }
 	  int newsize = 2*size;
-	  list = new stud*[newsize]; //recreate list to double
+	  list = new student*[newsize]; //recreate list to double
 	  for (int c = 0; c < newsize; c++) {
 	    list[c] = NULL;
 	  }
 	  //re-add all students to newly sized list
 	  for (int a = 0; a < size; a++) {
 	    if (temp[a] != NULL) {
-	      stud* move = temp[a];
+	      student* move = temp[a];
 	      if (move->next != NULL) {
-		stud* nxt = move->next;
+		student* nxt = move->next;
 		move->next = NULL;
 		nxt->prev = NULL;
 		ADD(list, nxt, newsize);
 		if (nxt->next != NULL) {
-		  stud* dnxt = nxt->next;
+		  student* dnxt = nxt->next;
 		  nxt->next = NULL;
 		  dnxt->prev = NULL;
 		  ADD(list, dnxt, newsize);
@@ -209,27 +209,27 @@ int main() {
 }
 
 //basically the hash function
-void ADD(stud** list, stud* newStud, int size) {
-  int index = (newStud->id) % size;
+void ADD(student** list, student* newStudent, int size) {
+  int index = (newStudent->id) % size;
   //put new student in current array
   if (list[index] == NULL) {
     //no colision, simply add to list
-    list[index] = newStud;
+    list[index] = newStudent;
   } else {
     //collision, then add as next ptr
     if ((list[index])->next == NULL) {
       //no previous collision
-      (list[index])->next = newStud;
+      (list[index])->next = newStudent;
       (list[index]->next)->prev = (list[index])->next;
     } else {
       //already a collision existent there
-      ((list[index])->next)->next = newStud;
+      ((list[index])->next)->next = newStudent;
       (((list[index])->next)->next)->prev = ((list[index])->next)->next;
     }
   }
 }
 
-bool COLL(stud** list, int size) {
+bool COLL(student** list, int size) {
   bool newlist = false;  //keep trak whether there's a need to make a new list
   int ctr = 0;
   while (newlist == false && ctr < size) {
@@ -245,15 +245,15 @@ bool COLL(stud** list, int size) {
   return newlist;
 }
 
-void PRINT(stud** list, int size) {
+void PRINT(student** list, int size) {
   for (int i = 0; i < size; i++) {
-    stud* curr = list[i];
+    student* curr = list[i];
     if (curr != NULL) {
       cout << curr->Fname << " ";
       cout << curr->Lname;
       cout << " #" << curr->id << " GPA:";
       cout << fixed << setprecision(2) << curr->gpa;
-      stud* nxt = curr->next;
+      student* nxt = curr->next;
       if (nxt != NULL) {
 	cout << '\t' << nxt->Fname << " ";
 	cout << nxt->Lname;
@@ -265,10 +265,10 @@ void PRINT(stud** list, int size) {
   }
 }
 
-void REMOVE(stud** list, int id, int size) {
+void REMOVE(student** list, int id, int size) {
   int i = id % size;
   if (list[i] == NULL) {
-    cout << endl << "No such student to remove." << endl;
+    cout << endl << "They don't exist :(" << endl;
   } else {
     if (list[i]->id == id) {
       //found then delete
@@ -276,19 +276,19 @@ void REMOVE(stud** list, int id, int size) {
 	list[i] = NULL; 
       } else {
 	//replace with next in collision
-	stud* newcurr = list[i]->next;
+	student* newcurr = list[i]->next;
 	newcurr->prev = NULL;
 	list[i] = newcurr;
       }
     } else {
       if (list[i]->next == NULL) {
-	cout << endl << "No such student to remove." << endl;
+	cout << endl << "They don't exist :(" << endl;
       } else {
 	if (list[i]->next->id == id) {
 	  //found then remove
 	  list[i]->next = NULL;
 	} else {
-	    cout << "No such student to remove." << endl;
+	    cout << "They don't exist :(" << endl;
 	}
       }
     }
